1bdb63c2b25d4f9e6efc04e024cad3742df8e85d
Atul_khadse <khadseatul078@gmail.com>
2025-08-08 23:01:26 +0530

    add all address feilds

--- Fronend/src/pages/AddAddress.jsx
+++ Fronend/src/pages/AddAddress.jsx
@@ -2,16 +2,16 @@ import React, { useState } from 'react'
 import address_man from '../assets/address_man.png'
 
 // input field component
-const InputField = ({ type, placeholder, name, handleChange, address}) => (
-    <input className='w-full px-2 py-2.5 border border-gray-500/30 rounded outline-none text-gray-500 focus:border-primary transition'
-     type={type} 
+const InputField = ({ type, placeholder, name, handleChange, address }) => (
+  <input className='w-full px-2 py-2.5 border border-gray-500/30 rounded outline-none text-gray-500 focus:border-primary transition'
+    type={type}
     placeholder={placeholder}
     onChange={handleChange}
     name={name}
     value={address[name]}
     required
 
-    />
+  />
 )
 
 
@@ -19,52 +19,74 @@ const AddAddress = () => {
 
 
   const [address, setAddress] = useState({
-    firstName:'',
-    lastName:'',
-    email:'',
-    street:'',
-    city:'',
-    state:'',
-    zipcode:'',
-    country:'',
-    phone:'',
+    firstName: '',
+    lastName: '',
+    email: '',
+    street: '',
+    city: '',
+    state: '',
+    zipcode: '',
+    country: '',
+    phone: '',
   })
 
 
-const handleChange = (e) => {
-  const { name, value } = e.target;
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-  setAddress((prevAddress) => ({
-    ...AddAddress,
-    [name]: value,
-  }))
-} 
-
-
+    setAddress((prevAddress) => ({
+      ...AddAddress,
+      [name]: value,
+    }))
+  }
 
 
-const onsubmitHandler = async (e) => {
+
+
+  const onsubmitHandler = async (e) => {
     e.preventDefault();
-}
+  }
 
 
   return (
     <div className='mt-16 pb-16'>
-      
-        <p className='text-2xl md:text-3xl text-gray-500'>Add Shipping <span className='font-medium text-primary'>Address</span></p>
-        <div className='flex flex-col-reverse md:flex-row justify-between mt-10'>
-            <div className='flex-1 max-w-md'>
-            <form onClick={onsubmitHandler} className='space-y-3 mt-6 text-sm'>
-              <div>
-                <InputField handleChange={handleChange} address={address} name="firstName" type="text" placeholder="first Name" />
-                <InputField handleChange={handleChange} address={address} name="lastName" type="text" placeholder="last Name" />
 
-              </div>
-            </form>
+      <p className='text-2xl md:text-3xl text-gray-500'>Add Shipping <span className='font-medium text-primary'>Address</span></p>
+      <div className='flex flex-col-reverse md:flex-row justify-between mt-10'>
+        <div className='flex-1 max-w-md'>
+          <form onClick={onsubmitHandler} className='space-y-3 mt-6 text-sm'>
+            <div className='grid grid-cols-2 gap-4'>
+              <InputField handleChange={handleChange} address={address} name="firstName" type="text" placeholder="First Name" />
+              <InputField handleChange={handleChange} address={address} name="lastName" type="text" placeholder="Last Name" />
             </div>
-            <img className='md:mr-16 mb-16 md:mt-0' src={address_man} alt="Add Address" />
+
+            <InputField handleChange={handleChange} address={address} name="email" type="email" placeholder="Email address" />
+            <InputField handleChange={handleChange} address={address} name="street" type="text" placeholder="Street" />
+
+            <div className='grid grid-cols-2 gap-4'>
+              <InputField handleChange={handleChange} address={address} name="city" type="text" placeholder="City" />
+              <InputField handleChange={handleChange} address={address} name="state" type="text" placeholder="State" />
+
+            </div>
+
+            <div className='grid grid-cols-2 gap-4'>
+              <InputField handleChange={handleChange} address={address} name="zipcode" type="number" placeholder="Zip code" />
+              <InputField handleChange={handleChange} address={address} name="country" type="text" placeholder="Country" />
+
+            </div>
+
+              <InputField handleChange={handleChange} address={address} name="phone" type="text" placeholder="Phone" />
+
+
+
+            <button className='w-full mt-6 bg-primary text-white py-3 hover:bg-primary/40 transition cursor-pointer uppercase'>
+              Save address
+            </button>
+          </form>
         </div>
-    
+        <img className='md:mr-16 mb-16 md:mt-0' src={address_man} alt="Add Address" />
+      </div>
+
     </div>
   )
 }
