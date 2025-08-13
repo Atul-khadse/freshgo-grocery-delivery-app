3cdab31402ca825f19cac5f6bca22e5bfb75d4be
Atul_khadse <khadseatul078@gmail.com>
2025-08-13 12:39:49 +0530

    useIffrect

--- Fronend/src/components/seller/SellerLogin.jsx
+++ Fronend/src/components/seller/SellerLogin.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useAppContext  } from '../../context/AppContext'
 
 const SellerLogin = () => {
 
     const [isSeller, setIsSeller, navigate] = useAppContext();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    useEffect(() => {
+      if(isSeller){
+        navigate("/seller")
+      }
+    }),[isSeller]
 
 
 
