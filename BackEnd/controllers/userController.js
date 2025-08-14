01c46676512392c705b51d9778f5ce30655dea56
Atul_khadse <khadseatul078@gmail.com>
2025-08-15 02:38:21 +0530

    login token

--- BackEnd/controllers/userController.js
+++ BackEnd/controllers/userController.js
@@ -68,6 +68,21 @@ export const login = async (req, res) => {
             return res.json({success: false, message:"Invalid eamil or password"})
             
         }
+
+
+        
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '7d'})
+
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            maxAge: 7 * 24 * 60 * 1000,
+        })
+
+
+                   return res.json({success: true, user: {email: user.email, name: user.name}})
+
     } catch (error) {
         
     }
