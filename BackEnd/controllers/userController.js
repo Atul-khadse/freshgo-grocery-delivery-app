2971af5669c127b4f951acaa121c37c80b70d55e
Atul_khadse <khadseatul078@gmail.com>
2025-08-15 03:03:11 +0530

    create logout fun

--- BackEnd/controllers/userController.js
+++ BackEnd/controllers/userController.js
@@ -107,3 +107,19 @@ export const isAuth = async (req , res) =>{
 
     }
 }
+
+
+
+// logout
+export const logout = async (req, res) =>{
+    try {
+        res.clearCookie('token',{
+              httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        })
+        return res.json({ success:true ,message:"logged out" })
+    } catch (error) {
+        
+    }
+}
