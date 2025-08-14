a052468d93b8437d1cdc7e682b42405523924256
Atul_khadse <khadseatul078@gmail.com>
2025-08-15 01:10:04 +0530

    add middleware config

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,8 +1,19 @@
+import cookieParser from 'cookie-parser';
 import express from 'express'
+import cors from 'cors'
 
 const app = express();
 
 const port = process.env.PORT || 4000;
+
+const allowedOrigins = ['http://localhost:5173']
+
+
+//middleware 
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({origin: allowedOrigins, creadentials: true}))
+
 
 app.get('/', (req, res) => res.send("Api is working"));
 
