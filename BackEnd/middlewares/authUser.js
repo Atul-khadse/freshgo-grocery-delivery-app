fd11e028ca945ba87774976a9c98747e84b79cec
Atul_khadse <khadseatul078@gmail.com>
2025-08-15 02:55:43 +0530

    auth middleware error handle

--- BackEnd/middlewares/authUser.js
+++ BackEnd/middlewares/authUser.js
@@ -19,6 +19,12 @@ const authUser = async (req , res ,next)=>{
         }
         next()
     } catch (error) {
-        
+            console.log(error.message)
+         res.json({ success: false, message: error.message })
+
     }
 }
+
+
+
+export default authUser;
