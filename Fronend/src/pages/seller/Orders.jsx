75faf99edc34f077a775124f3a20c688ad2b1ee9
Atul_khadse <khadseatul078@gmail.com>
2025-08-14 23:20:59 +0530

    create fechOrder fun

--- Fronend/src/pages/seller/Orders.jsx
+++ Fronend/src/pages/seller/Orders.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from 'react'
 import { useAppContext } from '../../context/AppContext'
+import { dummyOrders } from '../../assets/assets';
 
 const Orders = () => {
 
 const {currency } = useAppContext();
 const [orders, setOrders] = useState([]);
+
+
+const fetchOrders = async () => {
+  setOrders(dummyOrders)
+}
 
 
 
