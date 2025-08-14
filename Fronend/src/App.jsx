08cdd45a7a702d2e7b949d78341de98a4b92963c
Atul_khadse <khadseatul078@gmail.com>
2025-08-14 13:18:50 +0530

    seller route add

--- Fronend/src/App.jsx
+++ Fronend/src/App.jsx
@@ -13,6 +13,7 @@ import Cart from './pages/Cart.jsx'
 import AddAddress from './pages/AddAddress.jsx'
 import MyOrders from './pages/MyOrders.jsx'
 import SellerLogin from './components/seller/SellerLogin.jsx'
+import SellerLayout from './pages/seller/SellerLayout.jsx'
 
 
 
@@ -24,7 +25,7 @@ const App = () => {
   const { showUserLogin, isSeller } = useAppContext();
 
   return (
-    <div>
+    <div className='text-default min-h-screen text-gray-700 bg-white'>
      {isSellerPath ? null : <Navbar />}
      { showUserLogin ? <Login /> : null}
     <Toaster/>
@@ -37,7 +38,9 @@ const App = () => {
           <Route path='/cart' element={<Cart />} />
           <Route path='/add-address' element={<AddAddress />} />
           <Route path='/my-orders' element={<MyOrders />} />
-          <Route path='/seller' element={isSeller ? null : <SellerLogin />} />
+          <Route path='/seller' element={isSeller ? <SellerLayout/> : <SellerLogin />} />
+
+
 
 
 
