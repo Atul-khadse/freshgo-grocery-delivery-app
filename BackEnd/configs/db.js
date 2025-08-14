0068c30a67e135e030ef6353ccd2126ed4b71e95
Atul_khadse <khadseatul078@gmail.com>
2025-08-15 01:35:44 +0530

    create db connection

---
+++ BackEnd/configs/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try{
+
+        mongoose.connection.on('connected', ()=> console.log("db connected"))
+        await mongoose.connect(`${process.env.MONGODB_URL}/freshgoDB`)
+    }catch(error){
+        console.error(error.message);
+    }
+}
+
+
+export default connectDB;
