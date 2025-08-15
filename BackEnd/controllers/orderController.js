1fd97799756a190bf19d5cd4a7fe0326938f8e50
Atul_khadse <khadseatul078@gmail.com>
2025-08-15 18:20:34 +0530

    create orderRoute

--- BackEnd/controllers/orderController.js
+++ BackEnd/controllers/orderController.js
@@ -34,3 +34,37 @@ export const placeOrderCOD = async (req ,res) =>{
         
     }
 }
+
+
+
+// get orders by user id
+export const getUserOrders = async (req, res) =>{
+    try {
+        const { userId, } = req.body;
+        const orders = await Order.find({
+            userId,
+            $or: [{paymentType: "COD"}, {isPaid: true}]
+        }).populate("items.product address").sort({createdAt: -1})
+        res.json({ success: true, orders});
+    } catch (error) {
+        return res.json({ success: false, message: error.message})
+        
+    }
+}
+
+
+
+// get All orders for seller /admin
+
+export const getAllOrders = async (req, res) =>{
+    try {
+        const orders = await Order.find({
+            userId,
+            $or: [{paymentType: "COD"}, {isPaid: true}]
+        }).populate("items.product address").sort({createdAt: -1})
+        res.json({ success: true, orders});
+    } catch (error) {
+        return res.json({ success: false, message: error.message})
+        
+    }
+}
--- BackEnd/server.js
+++ BackEnd/server.js
@@ -9,6 +9,7 @@ import connectCloudinary from './configs/cloudinary.js';
 import productRouter from './routes/productRoute.js';
 import cartRoute from './routes/cartRoute.js';
 import addressRouter from './routes/addressRoute.js';
+import orderRouter from './routes/orderRoute.js';
 
 const app = express();
 
@@ -32,6 +33,8 @@ app.use('/api/seller', sellerRouter );
 app.use('/api/product', productRouter );
 app.use('/api/cart', cartRoute );
 app.use('/api/address', addressRouter );
+app.use('/api/order', orderRouter );
+
 
 
 
