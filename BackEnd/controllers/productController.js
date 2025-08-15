04695a7b8503d2415273502064c1376eeda0da7c
Atul_khadse <khadseatul078@gmail.com>
2025-08-15 16:59:35 +0530

    create productById fun for single product

--- BackEnd/controllers/productController.js
+++ BackEnd/controllers/productController.js
@@ -44,7 +44,14 @@ export const productList = async (req , res ) => {
 
 // get Single Product
 export const productById = async (req , res ) => {
-    
+    try {
+        const { id } = req.body;
+        const product = await Product.findById(id);
+        res.json({ success: true, product});
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false , message: error.message});
+    }
 }
 
 
