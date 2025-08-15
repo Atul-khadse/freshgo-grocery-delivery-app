3054ce58964c4642b6b918c08e67012a8ae9ceab
Atul_khadse <khadseatul078@gmail.com>
2025-08-15 17:12:50 +0530

    create cartController

---
+++ BackEnd/controllers/cartController.js
@@ -0,0 +1,20 @@
+
+import User from "../models/User";
+
+
+
+
+// Update User CartData
+
+export const updateCart = async (req , res) => {
+    try {
+        const { userId, cartItem } = req.body;
+        await User.findByIdAndUpdate(userId, {cartItem});
+        res.json({ success: true, message: "Cate Updatec"});
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message})
+        
+        
+    }
+}
