b6a1ef324fd05b49b08bb1df99c3888089724b42
Atul_khadse <khadseatul078@gmail.com>
2025-08-15 16:40:29 +0530

    creat cloudinary

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -5,6 +5,7 @@ import connectDB from './configs/db.js';
 import 'dotenv/config';
 import userRouter from './routes/userRoute.js';
 import sellerRouter from './routes/sellerRoute.js';
+import connectCloudinary from './configs/cloudinary.js';
 
 const app = express();
 
@@ -13,6 +14,7 @@ const port = process.env.PORT || 4000;
 const allowedOrigins = ['http://localhost:5173']
 
 await connectDB();
+await connectCloudinary();
 
 
 //middleware 
