28df1b16cad769979f92f1e457e6bef3f07063e8
Atul_khadse <khadseatul078@gmail.com>
2025-08-17 03:34:55 +0530

    created   // place order stripe in frontend

--- Fronend/src/pages/Cart.jsx
+++ Fronend/src/pages/Cart.jsx
@@ -66,6 +66,19 @@ const Cart = () => {
                 }else{
                     toast.error(data.message);
                 }
+            }else{
+                // place order stripe
+                 const { data } = await axios.post('/api/order/stripe',{
+                    userId: user._id,
+                    items: cartArray.map(item => ({product: item._id, quantity: item.quantity})),
+                    address: selectAddress._id
+                })
+
+                if (data.success) {
+                   window.location.replace(data.url)
+                }else{
+                    toast.error(data.message);
+                }
             }
 
         } catch (error) {
